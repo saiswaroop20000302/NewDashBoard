713f082c929b98d0055b1553a28633a7fa19f312
swaroop <swaroopss020@gmail.com>
2026-01-16 08:51:32 -0600

    Initial Commit

---
+++ NewDashBoardUITests/NewDashBoardUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  NewDashBoardUITestsLaunchTests.swift
+//  NewDashBoardUITests
+//
+//  Created by Veera Venkata Sai Swaroop Grandhi on 1/16/26.
+//
+
+import XCTest
+
+final class NewDashBoardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
